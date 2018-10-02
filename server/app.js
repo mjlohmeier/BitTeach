50be985d5f0a788bc9ff53aae17cc1c4cb7cbb77
Matheus Duarte <mfduar8766@gmail.com>
2018-10-02 12:00:56 -0400

    User registration.

--- server/app.js
+++ server/app.js
@@ -3,10 +3,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const users = require('./routes/api/users');
+
 const app = express();
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/api/users',users);
 
 app.listen(port, () => console.log(`listening on port: ${port}`));
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,4 +1,71 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
+const { validateToken } = require("./validateUser");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JwtPassword } = require("../../database");
+
+const { DB } = require("../../database");
+
+routes.post("/register", (req, res) => {
+  bcrypt.hash(req.body.user_password, 10, (err, hash) => {
+    if (err) {
+      return res.status(500).json({ msg: err });
+    } else {
+      let newUser = {
+        email: req.body.email,
+        user_password: hash
+      };
+      DB.one(
+        `INSERT INTO users (email, user_password) VALUES ($1, $2)RETURNING *;`,
+        [newUser.email, newUser.user_password]
+      )
+        .then(data => {
+          let token = jwt.sign({ id: data.id }, JwtPassword, {
+            expiresIn: "1d"
+          });
+          res.send({ token });
+        })
+        .catch(err => {
+          console.log(err);
+          res.send({ error: err });
+        });
+    }
+  });
+});
+
+routes.post("/login", validateToken, (req, res) => {
+  let users = {
+    email: req.body.email,
+    user_password: req.body.user_password
+  };
+  DB.one(
+    `SELECT 
+ *
+FROM
+ users
+WHERE
+email = $1;`,
+    [users.email]
+  ).then(data => {
+    console.log(data);
+    bcrypt
+      .compare(req.body.user_password, data.user_password, (err, success) => {
+        if (err) {
+          return res.status(500).json({ err: err });
+        }
+        if (success) {
+          console.log(data);
+          let token = jwt.sign({ id: data.id }, JwtPassword, {
+            expiresIn: "1d"
+          });
+          res.send({ token });
+        }
+      })
+  }).catch(err => {
+    console.log(err);
+    res.send({ error: err });
+  });
+});
 
 module.exports = routes;
