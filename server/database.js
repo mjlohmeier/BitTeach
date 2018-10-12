37e5b61479449a1448796aca37e1eb0b52db148f
Matheus Duarte <mfduar8766@gmail.com>
2018-10-12 14:10:24 -0400

    Fixed ENV.

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ dotEnv.config();
 const pg = require("pg-promise")();
 const DBConfig = `postgres://${process.env.USERNAME}:${
   process.env.PASSWORD
-}@bitTeach.teach:5432/bitTeach`;
+}@localhost:5432/bitTeach`;
 const DB = pg(DBConfig);
 const JwtPassword = "password";
 
