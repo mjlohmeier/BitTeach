1828a9a17e1d75cb99d6ef074f903e17151bd138
Matheus Duarte <mfduar8766@gmail.com>
2018-10-12 14:12:25 -0400

    changes

--- bit-teach/src/Classes/UserAuthRegisterPage.js
+++ bit-teach/src/Classes/UserAuthRegisterPage.js
@@ -18,7 +18,7 @@ class UserAuthRegisterPage extends React.Component {
     const { dispatch } = this.props;
 
     let registerUser = user => {
-      return fetch("http://localhost:5000/api/users/register", {
+      return fetch(`${process.env.REACT_API_URLS}/api/users/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
