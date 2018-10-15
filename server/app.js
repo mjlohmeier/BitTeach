106b20bb09b7bdb2462389728944b633b5c1d83a
Michael Lohmeier <mjlohmeier@gmail.com>
2018-10-15 11:48:14 -0400

    moved around statics in app.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
 //reminder: go back to nginx config
 
 const express = require('express');
-const static = express.static;
-app.use(static('../client/build'));
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -12,6 +10,8 @@ const currency = require('./routes/api/currency');
 const wallet = require('./routes/api/wallet');
 
 const app = express();
+const static = express.static;
+app.use(static('../client/build'));
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
